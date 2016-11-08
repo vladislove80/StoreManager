5ecb1679030310e63b0c0dd67e5f4fcba9009d9c
vladislove80 <vladislove.n.u@gmail.com>
2016-11-08 18:15:35 +0200

    smth sht

--- app/src/main/java/storemanager/com/app/MainActivity.java
+++ app/src/main/java/storemanager/com/app/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //some change
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
