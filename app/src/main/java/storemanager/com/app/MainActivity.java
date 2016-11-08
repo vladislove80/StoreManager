46142bb1a360159199c211542d3a0274cdabaeea
vladislove80 <vladislove.n.u@gmail.com>
2016-11-08 17:49:01 +0200

    add main activity

---
+++ app/src/main/java/storemanager/com/app/MainActivity.java
@@ -0,0 +1,14 @@
+package storemanager.com.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //some change
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
