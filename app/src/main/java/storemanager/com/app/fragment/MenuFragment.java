7b60fc7912c372ae0e74b477a6e7fca6655777ec
vladislove80 <vladislove.n.u@gmail.com>
2017-03-06 19:51:12 +0200

    Working with Menu

--- app/src/main/java/storemanager/com/app/fragment/MenuFragment.java
+++ app/src/main/java/storemanager/com/app/fragment/MenuFragment.java
@@ -1,5 +1,6 @@
 package storemanager.com.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,21 +13,28 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import storemanager.com.app.R;
+import storemanager.com.app.activity.AddBaseIngredientActivity;
+import storemanager.com.app.activity.AddMenuItemActivity;
 import storemanager.com.app.adapter.MenuFragmentAdapter;
 import storemanager.com.app.models.CoffeItem;
 
 public class MenuFragment extends Fragment {
     public static final String TAG = MenuFragment.class.getSimpleName();
+    public final static int REQ_CODE_ADD_ITEM = 2;
+    public final static int REQ_CODE_ADD_INGREDIENT = 3;
 
     private int mPage;
     private RelativeLayout noDataLayout;
-    private Button addButtn;
+    private Button addMenuButton;
+    private Button addIngredientsButton;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private TextView menuLabel;
 
     private List<CoffeItem> mDataset;
 
@@ -42,6 +50,7 @@ public class MenuFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //mPage = getArguments().getInt(TAG);
+        mDataset = new ArrayList<>();
     }
 
     @Nullable
@@ -49,18 +58,46 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu_fragment, container, false);
 
-        addButtn = (Button) view.findViewById(R.id.add_menu_item_button);
+        addMenuButton = (Button) view.findViewById(R.id.add_menu_item_button);
+        addIngredientsButton = (Button) view.findViewById(R.id.add_ingredients_button);
+        menuLabel = (TextView) view.findViewById(R.id.menu_fragment_label);
+
+
+        addMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddMenuItemActivity.class);
+                startActivityForResult(intent, REQ_CODE_ADD_ITEM);
+            }
+        });
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.menu_recycler_view);
-
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new MenuFragmentAdapter(getContext(), mDataset);
         mRecyclerView.setAdapter(mAdapter);
 
-        noDataLayout = (RelativeLayout) view.findViewById(R.id.no_menu_data_layout);
-        noDataLayout.setVisibility(View.GONE);
+        /*noDataLayout = (RelativeLayout) view.findViewById(R.id.no_menu_data_layout);
+        noDataLayout.setVisibility(View.GONE);*/
+
+        addIngredientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddBaseIngredientActivity.class);
+                startActivityForResult(intent, REQ_CODE_ADD_INGREDIENT);
+                //Toast.makeText(getContext(), "Ингридиенты", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data != null && requestCode == REQ_CODE_ADD_ITEM) {
+            /*mDataset.add();
+            mAdapter.notifyDataSetChanged();*/
+        }
     }
 }
