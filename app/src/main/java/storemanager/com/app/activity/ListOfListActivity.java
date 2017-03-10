bcc27931a0917a86e7f44a0326be83812c6b8178
vladislove80 <vladislove.n.u@gmail.com>
2017-03-10 19:57:54 +0200

    Test QuickAction

--- app/src/main/java/storemanager/com/app/activity/ListOfListActivity.java
+++ app/src/main/java/storemanager/com/app/activity/ListOfListActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import storemanager.com.app.ActionItem;
+import storemanager.com.app.QuickAction;
 import storemanager.com.app.R;
 
 public class ListOfListActivity extends AppCompatActivity {
@@ -48,5 +50,74 @@ public class ListOfListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Add action item
+        ActionItem addAction = new ActionItem();
+
+        addAction.setTitle("Phone");
+        //addAction.setIcon(getResources().getDrawable(R.drawable.phone));
+
+        // Accept action item
+        ActionItem accAction = new ActionItem();
+
+        accAction.setTitle("Gmail");
+        //accAction.setIcon(getResources().getDrawable(R.drawable.gmail));
+
+        // Upload action item
+        ActionItem upAction = new ActionItem();
+
+        upAction.setTitle("Talk");
+        //upAction.setIcon(getResources().getDrawable(R.drawable.talk));
+
+        final QuickAction mQuickAction = new QuickAction(this);
+
+        mQuickAction.addActionItem(addAction);
+        mQuickAction.addActionItem(accAction);
+        mQuickAction.addActionItem(upAction);
+
+        // setup the action item click listener
+        mQuickAction
+                .setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
+                    public void onItemClick(int pos) {
+
+                        /*if (pos == 0) { // Add item selected
+                            Toast.makeText(MainActivity.this,
+                                    "PHONE item selected", Toast.LENGTH_SHORT)
+                                    .show();
+                        } else if (pos == 1) { // Accept item selected
+                            Toast.makeText(MainActivity.this,
+                                    "GMAIL item selected", Toast.LENGTH_SHORT)
+                                    .show();
+                        } else if (pos == 2) { // Upload item selected
+                            Toast.makeText(MainActivity.this, "TALK selected",
+                                    Toast.LENGTH_SHORT).show();
+                        }*/
+                    }
+                });
+
+        /*ImageView ivPic1 = (ImageView) this.findViewById(R.id.ivPic1);
+        ivPic1.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mQuickAction.show(v);
+                mQuickAction.setAnimStyle(QuickAction.ANIM_GROW_FROM_CENTER);
+            }
+        });*/
+
+        /*Button btClickMe = (Button) this.findViewById(R.id.button1);
+        btClickMe.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mQuickAction.show(v);
+                mQuickAction.setAnimStyle(QuickAction.ANIM_GROW_FROM_CENTER);
+            }
+        });*/
+
+        itemNamesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQuickAction.show(v);
+                mQuickAction.setAnimStyle(QuickAction.ANIM_GROW_FROM_CENTER);
+            }
+        });
     }
+
 }
