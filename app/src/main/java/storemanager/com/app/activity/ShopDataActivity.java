68f86459f34922074377ceabd84150eb1e135fc9
vladislove80 <vladislove.n.u@gmail.com>
2017-04-25 12:29:07 +0300

    Small improvement

--- app/src/main/java/storemanager/com/app/activity/ShopDataActivity.java
+++ app/src/main/java/storemanager/com/app/activity/ShopDataActivity.java
@@ -22,6 +22,7 @@ public class ShopDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shop_data);
         Intent intent = getIntent();
         setShopName(intent.getStringExtra(TAG));
+        setTitle(shopName);
         setViewPager();
     }
 
